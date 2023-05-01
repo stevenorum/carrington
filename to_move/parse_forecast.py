200cde3bf9f5da9b4e237e5d669d4e95a54a486a
stevenorum <stevenorum@gmail.com>
2023-05-01 15:18:58 -0400

    Update parse_forecast.py
    Fix handling of the Ap forecast lines now that I know what that even means (https://www.ngdc.noaa.gov/stp/geomag/kp_ap.html)

--- to_move/parse_forecast.py
+++ to_move/parse_forecast.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import requests
 
 FORECAST_URL = "https://services.swpc.noaa.gov/text/3-day-geomag-forecast.txt"
 
-EASY_PREFIXES = [":Product: ",":Issued: ","Observed","Estimated","Predicted",]
+EASY_PREFIXES = [":Product: ",":Issued: ",]
 STORM_KEY = "storm_forecasts"
 KP_KEY = "kp_forecasts"
+AP_KEY = "ap_forecasts"
 
 def clean_key(k):
     k = k.lower()
@@ -45,15 +46,18 @@ t = requests.get(FORECAST_URL).text.replace("\r","")
 days = [x.strip() for x in t.split("NOAA Kp index forecast")[1].split("\n")[1].strip().split("  ") if x.strip()]
 issued = datetime.strptime(t.split(":Issued:")[1].split("\n")[0].strip(), "%Y %b %d %H%M %Z") # 2023 Apr 30 2205 UTC
 days_dt = [get_dt(issued, day) for day in days]
+today = days_dt[0] - timedelta(days=1)
+yesterday = today - timedelta(days=1)
 days_s = [date_to_s(dt) for dt in days_dt]
-for day in days:
-    print(get_dt(issued, day))
+today_s = date_to_s(today)
+yesterday_s = date_to_s(yesterday)
 
 info = {"raw":t}
 lines = clean_lines(t)
 last_line_handled = -1
 info[STORM_KEY] = {d:{} for d in days_s}
 info[KP_KEY] = {d:{} for d in days_s}
+info[AP_KEY] = {d:{} for d in [yesterday_s,today_s]+days_s}
 for i in range(len(lines)):
     if i <= last_line_handled:
         continue
@@ -61,9 +65,13 @@ for i in range(len(lines)):
     for prefix in EASY_PREFIXES:
         if line.startswith(prefix):
             info[clean_key(prefix)] = line[len(prefix):].strip()
-            if info[clean_key(prefix)].startswith("Ap "):
-                # I'm not positive, but I think this stands for "approximately" or something like that.
-                info[clean_key(prefix)] = info[clean_key(prefix)][3:]
+    if line.startswith("NOAA Ap Index Forecast"):
+        # https://www.ngdc.noaa.gov/stp/geomag/kp_ap.html
+        info[AP_KEY][yesterday_s] = lines[i+1].split(" ")[-1]
+        info[AP_KEY][today_s] = lines[i+2].split(" ")[-1]
+        apf = lines[i+3].split(" ")[-1].split("-")
+        for d in range(3):
+            info[AP_KEY][days_s[d]] = apf[d]
     if line.startswith("NOAA Geomagnetic Activity Probabilities"):
         active_pct = lines[i+1].split(" ")[-1].split("/")
         minor_pct = lines[i+2].split(" ")[-1].split("/")
